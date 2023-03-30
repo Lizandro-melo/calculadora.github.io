82df40a01fa2592d7f518d6634ddd86097da766c
Lizandro-Melo <joselizandro@souunisuam.com.br>
2023-03-30 13:25:27 -0300

    Atualização

--- js/buttonsAction.js
+++ js/buttonsAction.js
@@ -8,9 +8,9 @@ const botoesClass = document.querySelectorAll(".buttons")
 
 //Laço que mostra cada carácter clicado no display de cima
 for(let i = 0; i < botoesClass.length; i++){
-    /*botoesClass[i].addEventListener("click", function(){
+    botoesClass[i].addEventListener("click", function(){
         verificarBotao(botoesClass[i], displayCalcular,displayResultado)
-    })*/
+    })
 
     botoesClass[i].addEventListener("touchstart", function(){
         verificarBotao(botoesClass[i], displayCalcular,displayResultado)
